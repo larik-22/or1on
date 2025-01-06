a85d54409c31ee51dc92379c918ae8d34339a572
Ignat Mamedov <548535@student.saxion.nl>
2025-01-06 14:33:57 +0100

    Added lint for front-end

---
+++ client/eslint.config.js
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+import globals from "globals";
+import svelteParser from "svelte-eslint-parser";
+import tsEslint from "typescript-eslint";
+
+export default tsEslint.config(
+    js.configs.recommended,
+    ...tsEslint.configs.recommended,
+    ...eslintPluginSvelte.configs["flat/recommended"],
+    eslintConfigPrettier,
+    ...eslintPluginSvelte.configs["flat/prettier"],
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: { ...globals.node, ...globals.browser },
+            parser: svelteParser,
+            parserOptions: {
+                parser: tsEslint.parser,
+                extraFileExtensions: [".svelte"],
+            },
+        },
+    },
+    {
+        ignores: [
+            "**/build",
+            "**/node_modules",
+            "**/package",
+        ],
+    },
+);
