51c4a6e3a361abc50a81fe3b44f6386be9501474
Robert Pelin <532136@student.saxion.nl>
2025-01-08 21:42:58 +0000

    hot fix on updating pass and username

--- server/src/routes/userDashboard.ts
+++ server/src/routes/userDashboard.ts
@@ -3,6 +3,9 @@ import { isLoggedIn } from '../middleware/isLoggedIn.js';
 import {  updateUsername, updateUserPassword } from '../controllers/userController.js';
 import {EntityManager} from "@mikro-orm/core";
 import logger from "../utils/logger.js";
+import type {User} from "../models/user.js";
+import bcrypt from "bcryptjs";
+
 
 const userDashboard = new Hono();
 
@@ -14,11 +17,16 @@ const userDashboard = new Hono();
  */
 userDashboard.post('/update-username', isLoggedIn, async (c) => {
     const em = c.get('em' as 'jwtPayload') as EntityManager;
+    const payload = c.get('jwtPayload') as User;
     const { oldUsername, newUsername } = await c.req.json();
     logger.info('Update user request received', { oldUsername, newUsername });
     if (!oldUsername || !newUsername) {
         return c.json({ success: false, message: 'Missing required fields' }, 400);
     }
+    if(payload.username !== oldUsername){
+        return c.json({ success: false, message: 'Old username does not match' }, 400);
+    }
+
 
     const result = await updateUsername(em,oldUsername, newUsername);
     return c.json(result, result.success ? 200 : 400);
@@ -32,11 +40,27 @@ userDashboard.post('/update-username', isLoggedIn, async (c) => {
  */
 userDashboard.post('/update-password', isLoggedIn, async (c) => {
     const em = c.get('em' as 'jwtPayload') as EntityManager;
-    const { userId, oldPassword, newPassword } = await c.req.json();
+    const payload = await c.get('jwtPayload') as User;
+    const { oldPassword, newPassword } = await c.req.json();
 
-    if (!userId || !oldPassword || !newPassword) {
+    if (!oldPassword || !newPassword) {
         return c.json({ success: false, message: 'Missing required fields' }, 400);
     }
+    const userId = payload.id;
+    if(oldPassword === newPassword){
+        return c.json({ success: false,
+            message: 'New password cannot be the same as the old password' }
+            ,400)
+    }
+    bcrypt.compare(oldPassword, payload.password, (err, res) => {
+        if(!res){
+            return c.json({ success: false, message: 'Old password is incorrect' }, 400);
+        }
+    });
+    if(newPassword.length < 5){
+        return c.json({ success: false, message: 'Password must be at least 6 characters' }, 400);
+    }
+
 
     const result = await updateUserPassword(
         em,
